8ccf82fce5496f92ffd21ea3b7799b8bdae31b04
anjianshi <anjianshi@gmail.com>
2024-04-10 22:13:27 +0800

    update deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,3 @@
 module.exports = {
   extends: [require.resolve('@anjianshi/presets-eslint-node')],
-  rules: {
-    'no-await-in-loop': 'off',
-    'no-constant-condition': ['error', { checkLoops: false }],
-    'no-loop-func': 'off',
-    '@typescript-eslint/no-loop-func': 'off',
-  },
 }
